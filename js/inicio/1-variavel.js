5a668e3c331c267242b84b522a9093c4c0dffcc0
DARQUIO WENDEL RODRIGUES OLIVEIRA <DARQUIO1007@DIGITALCOLLEGE.LOCAL>
2024-05-16 21:52:15 -0300

    Iniciando com o JavaScript

---
+++ js/inicio/1-variavel.js
@@ -0,0 +1,50 @@
+/*     Chamar variável: 
+         var, const, let          */
+
+
+// Criando a variável e atribuindo o valor
+let nome = Dárquio;
+console.log(nome);
+nome = 30;
+console.log(nome);
+nome = 10;
+console.log(nome);
+
+
+/* 
+1- Não pode ser palavra reservada
+2- Não pode iniciar com número
+3- Não pode ter espaço vazio 
+4- Não pode ter caracteres especiais
+5- Não pode ser uma palavra reservada   
+6- Não pode iniciar com operadores aritméticos: + , - , / , *
+7- Não pode iniciar com símbolos: ! , $ , % , ^ , & , * , ( , ) , { , }
+*/
+
+// let nome = 'olá';
+// let  nome5 = 'James';
+// let sobre_nome = 'Wndel';
+
+/*
+CamelCase: inicia a 1º palavra em minúsculo e o restante das palavras iniciais em maiúsculas
+*/
+
+
+/*
+let #idade = 12 ;  //erro
+let _idade = 12;   //erro
+let $idade2 = 12;  //erro
+*/
+
+// Alterando um valor
+
+
+// Criando uma constante
+const alturaEverest = 8848;
+// Atribuindo o valor da variável
+alturaEverest = 10500;
+// Imprimindo o valor da variável
+console.log(alturaEverest);
+
+
+
