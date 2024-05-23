57e293112b4b6cc41770f1f5f35d7322fe985be2
DARQUIO WENDEL RODRIGUES OLIVEIRA <DARQUIO1007@DIGITALCOLLEGE.LOCAL>
2024-05-23 20:54:33 -0300

    Calcular o somar e função chamando função

---
+++ js/script2.js
@@ -0,0 +1,21 @@
+/* 
+Objetivo: Calcular a média de 2 números
+
+1- Pegar os dois números
+2- Somar os dois números
+3- Dividir a soma dos números pela quantidade de números
+*/
+let n1 = 900;
+let n2 = 100;
+let resultado = n1+n2;
+let resultado_media = resultado/2
+
+    function soma() {
+        console.log(resultado);
+    }
+     soma()
+
+    function media() {
+        console.log(resultado_media);
+    }
+    media()
