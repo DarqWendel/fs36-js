e76f4392b2df1b89175023f801a2c0b4daa3fe64
DARQUIO WENDEL RODRIGUES OLIVEIRA <DARQUIO1007@DIGITALCOLLEGE.LOCAL>
2024-06-06 21:29:03 -0300

    Melhorando a calculadora

--- js/script.js
+++ js/script.js
@@ -1,81 +1,74 @@
 // console.log('Script via body', document.getElementById('n1'))
 
-function soma() {
+const numero1 = document.getElementById('n1');
+const numero2 = document.getElementById('n2');
 
-    // Pegar os números dos inputs do HTML e converter para Float, pois será possivel fazer o cálculo com inteiros e decimais
+
+function soma() {
+   
     let numero1 = parseFloat(document.getElementById('n1').value);
     let numero2 = parseFloat(document.getElementById('n2').value);
-
-    //Pegando o resultado do cálculo e colocando na variável resultado
+    
     let resultado = (numero1) + (numero2);
-
     console.log(resultado);
-
-    // Pegando o elemento HTML e colocando na variável conteudoDiv
-   let conteudoDiv = document.getElementsByClassName('resultado')[0];
-
-   // Pegando o conteudoDiv acessando a prop innerHTML e inserindo o valor do calculo, a variavel
-   conteudoDiv.innerHTML = resultado
-
+  
+    let conteudoDiv = document.getElementsByClassName('resultado')[0];
    
+    conteudoDiv.innerHTML = resultado
 }
 
 function sub() {
 
-    // Pegar os números dos inputs do HTML e converter para Float, pois será possivel fazer o cálculo com inteiros e decimais
+    
     let numero1 = parseFloat(document.getElementById('n1').value);
     let numero2 = parseFloat(document.getElementById('n2').value);
 
-    //Pegando o resultado do cálculo e colocando na variável resultado
     let resultado = (numero1) - (numero2);
 
     console.log(resultado);
 
-    // Pegando o elemento HTML e colocando na variável conteudoDiv
-   let conteudoDiv = document.getElementsByClassName('resultado')[0];
+    let conteudoDiv = document.getElementsByClassName('resultado')[0];
 
-   // Pegando o conteudoDiv acessando a prop innerHTML e inserindo o valor do calculo, a variavel
-   conteudoDiv.innerHTML = resultado
+    conteudoDiv.innerHTML = resultado
 
-   
 }
 
 function mult() {
 
-    // Pegar os números dos inputs do HTML e converter para Float, pois será possivel fazer o cálculo com inteiros e decimais
     let numero1 = parseFloat(document.getElementById('n1').value);
     let numero2 = parseFloat(document.getElementById('n2').value);
-
-    //Pegando o resultado do cálculo e colocando na variável resultado
+   
     let resultado = (numero1) * (numero2);
 
     console.log(resultado);
-
-    // Pegando o elemento HTML e colocando na variável conteudoDiv
-   let conteudoDiv = document.getElementsByClassName('resultado')[0];
-
-   // Pegando o conteudoDiv acessando a prop innerHTML e inserindo o valor do calculo, a variavel
-   conteudoDiv.innerHTML = resultado
-
    
+    let conteudoDiv = document.getElementsByClassName('resultado')[0];
+
+    conteudoDiv.innerHTML = resultado
+
+
 }
 
 function div() {
 
-    // Pegar os números dos inputs do HTML e converter para Float, pois será possivel fazer o cálculo com inteiros e decimais
     let numero1 = parseFloat(document.getElementById('n1').value);
     let numero2 = parseFloat(document.getElementById('n2').value);
 
-    //Pegando o resultado do cálculo e colocando na variável resultado
     let resultado = (numero1) / (numero2);
 
     console.log(resultado);
 
-    // Pegando o elemento HTML e colocando na variável conteudoDiv
-   let conteudoDiv = document.getElementsByClassName('resultado')[0];
+    let conteudoDiv = document.getElementsByClassName('resultado')[0];
 
-   // Pegando o conteudoDiv acessando a prop innerHTML e inserindo o valor do calculo, a variavel
-   conteudoDiv.innerHTML = resultado
+    conteudoDiv.innerHTML = resultado
 
-   
+
 }
+
+    function limpar() {
+        document.getElementById('n1').value = ''; // Limpa o valor do primeiro input
+        document.getElementById('n2').value = ''; // Limpa o valor do segundo input
+        document.querySelector('.resultado').innerHTML = ''; // Limpa o conteúdo da div de resultado
+    }
+    
+
